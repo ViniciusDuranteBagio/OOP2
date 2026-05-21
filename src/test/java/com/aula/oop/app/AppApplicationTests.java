52496637c46fa5bec20360bc49796c40008f5396
aluno.unibave <thiagolucianocesconeto@gmail.com>
2026-05-20 21:42:42 -0300

    primeiro commit

--- src/test/java/com/aula/oop/app/AppApplicationTests.java
+++ src/test/java/com/aula/oop/app/AppApplicationTests.java
@@ -1,5 +1,6 @@
 package com.aula.oop.app;
 
+import com.aula.oop.app.model.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class AppApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
