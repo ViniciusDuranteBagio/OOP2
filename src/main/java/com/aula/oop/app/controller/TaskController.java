a36f61263a0a48395a9e05f513604536c28f7a4f
duurids <eduardojacobcardoso@gmail.com>
2026-06-09 19:16:07 -0300

    atualizado

---
+++ src/main/java/com/aula/oop/app/controller/TaskController.java
@@ -0,0 +1,29 @@
+package com.aula.oop.app.controller;
+
+import com.aula.oop.app.dto.TaskDTO;
+import com.aula.oop.app.dto.TaskResponseDTO;
+import com.aula.oop.app.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tarefa")
+public class TaskController {
+    TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<TaskResponseDTO> listAll() {
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public TaskResponseDTO createTask(@RequestBody @Valid TaskDTO tarefa) {
+        return taskService.createTask(tarefa);
+    }
+}
