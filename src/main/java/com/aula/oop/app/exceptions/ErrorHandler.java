33497a2aae571c0d241c541c1b26c66b60bf48f6
vinicius.durante <vinicius@kraftworks.com.br>
2026-06-09 21:43:16 -0300

    adicionando resto das coisas

--- src/main/java/com/aula/oop/app/exceptions/ErrorHandler.java
+++ src/main/java/com/aula/oop/app/exceptions/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.aula.oop.app.exceptions;
 
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,13 +11,9 @@ import java.util.Map;
 @ControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity resposta400(BadRequestException ex) {
-        Map body = Map.of(
-                "status", 404,
-                "mensagem", ex.getMessage()
-        );
-
-        return ResponseEntity.status(400).body(body);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> respostaValidacao(MethodArgumentNotValidException ex) {
+        String mensagem = ex.getFieldError().getDefaultMessage();
+        return ResponseEntity.badRequest().body(Map.of("status", 400, "mensagem", mensagem));
     }
 }
