56bcf3babea16d725779a8c1d6a3f3e5defd2171
Ruan <ruancassiomorgan24@gmail.com>
2026-06-17 21:32:51 -0300

    1

---
+++ src/main/java/com/aula/oop/app/service/LivroService.java
@@ -0,0 +1,63 @@
+package com.aula.oop.app.service;
+
+import com.aula.oop.app.dto.LivroDTO;
+import com.aula.oop.app.dto.LivroResponseDTO;
+import com.aula.oop.app.model.Livro;
+import com.aula.oop.app.repository.LivroRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LivroService {
+
+    LivroRepository livroRepository;
+
+    public LivroService(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    public List<Livro> getAllLivros() {
+        return livroRepository.findAll();
+    }
+
+    public Livro getLivro(Long id) {
+        return livroRepository.findById(id).orElseThrow();
+    }
+
+    public Livro create(Livro entity) {
+        livroRepository.save(entity);
+        return entity;
+    }
+
+    public Livro convertDTOToEntity(LivroDTO dto) {
+        Livro entity = new Livro();
+        entity.setId(dto.getId());
+        entity.setTitulo(dto.getTitulo());
+        entity.setPreco(dto.getPreco());
+        entity.setCodigo(dto.getCodigo());
+        entity.setAutor(dto.getAutor());
+        entity.setAnoPublicacao(dto.getAnoPublicacao());
+        return entity;
+    }
+
+    public LivroResponseDTO convertEntityToResponseDTO(Livro entity) {
+        LivroResponseDTO dto = new LivroResponseDTO();
+        dto.setId(entity.getId());
+        dto.setTitulo(entity.getTitulo());
+        dto.setPreco(entity.getPreco());
+        dto.setCodigo(entity.getCodigo());
+        dto.setAutor(entity.getAutor());
+        dto.setAnoPublicacao(entity.getAnoPublicacao());
+        return dto;
+    }
+
+    public void delete(Long id) {
+        livroRepository.deleteById(id);
+    }
+
+    public Livro put(Long id, LivroDTO livro) {
+        Livro livroToPut = livroRepository.findById(id).orElseThrow();
+        return livroRepository.save(livroToPut);
+    }
+}
