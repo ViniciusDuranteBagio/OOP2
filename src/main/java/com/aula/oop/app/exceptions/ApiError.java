53433156ae5866b5ef2d191dbe92a20ce16fc9ae
Gabriel Baggio Antunes <Baggiog138@gmail.com>
2026-06-23 19:31:41 -0300

    aula

---
+++ src/main/java/com/aula/oop/app/exceptions/ApiError.java
@@ -0,0 +1,15 @@
+package com.aula.oop.app.exceptions;
+
+import java.time.OffsetDateTime;
+import java.util.Map;
+
+import java.time.OffsetDateTime;
+import java.util.Map;
+
+public record ApiError(
+        Integer status,
+        String error,
+        String message,
+        Map<String, String> validationErrors,
+        OffsetDateTime timeStamp) {
+}
