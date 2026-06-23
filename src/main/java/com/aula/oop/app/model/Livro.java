f2b08932c01017376cdef4c942abbb35b1b6c0d0
Henri Zomer <henrizomer7@gmail.com>
2026-06-23 14:58:29 -0300

    usando lombok para facilitar

--- src/main/java/com/aula/oop/app/model/Livro.java
+++ src/main/java/com/aula/oop/app/model/Livro.java
@@ -1,73 +1,25 @@
 package com.aula.oop.app.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 import java.math.BigDecimal;
 
 @Entity
+@Table(name = "livros")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Livro {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
-
+    private String titulo;
     private String autor;
 
-    private String titulo;
-
     @Column(unique = true)
-
     private String codigo;
-
     private Integer anoPublicacao;
-
     private BigDecimal preco;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public Integer getAnoPublicacao() {
-        return anoPublicacao;
-    }
-
-    public void setAnoPublicacao(Integer anoPublicacao) {
-        this.anoPublicacao = anoPublicacao;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
 }
