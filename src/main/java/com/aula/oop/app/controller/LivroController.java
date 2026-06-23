1dd511adbbd20d0a1d0aa14ee432c9f3e68c1afc
Henri Zomer <henrizomer7@gmail.com>
2026-06-23 17:20:21 -0300

    fazendo controller

--- src/main/java/com/aula/oop/app/controller/LivroController.java
+++ src/main/java/com/aula/oop/app/controller/LivroController.java
@@ -1,4 +1,48 @@
 package com.aula.oop.app.controller;
 
+import com.aula.oop.app.dto.LivroRequestDTO;
+import com.aula.oop.app.dto.LivroResponseDTO;
+import com.aula.oop.app.service.LivroService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/livros")
 public class LivroController {
+
+    private final LivroService service;
+
+    public LivroController(LivroService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<LivroResponseDTO> salvar(@RequestBody LivroRequestDTO dto) {
+        return ResponseEntity.ok(service.salvar(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LivroResponseDTO>> listar() {
+        return ResponseEntity.ok(service.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LivroResponseDTO> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LivroResponseDTO> atualizar(
+            @PathVariable Long id,
+            @RequestBody LivroRequestDTO dto) {
+        return ResponseEntity.ok(service.atualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
