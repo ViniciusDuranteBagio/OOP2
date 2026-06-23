28ae6770bf4a43da895ed99ab0cefd0ff7fd00df
kavio12 <srprosinho1@gmail.com>
2026-06-23 19:20:44 -0300

    Notebook

--- src/main/java/com/aula/oop/app/controller/LivroController.java
+++ src/main/java/com/aula/oop/app/controller/LivroController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/livros")
+@RequestMapping("/api/livros")
 @RequiredArgsConstructor
 public class LivroController {
 
